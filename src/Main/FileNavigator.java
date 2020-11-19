bc788d64c22ca9e97b50597c94daad696dae291a
c1i2b <jeremylsellors@gmail.com>
2020-11-18 16:08:58 -0800

    Filenavigator Created

---
+++ src/Main/FileNavigator.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.util.ArrayList;
+
+public class FileNavigator {
+
+    public void getPaths(String folderPath, ArrayList<String> paths) {
+        File folder = new File(folderPath);
+        File[] fileList = folder.listFiles();
+        for (File file : fileList) {
+            if (file.isFile()) {
+                paths.add(file.getAbsolutePath());
+            } else if (file.isDirectory()) {
+                getPaths(file.getAbsolutePath(), paths);
+            }
+        }
+    }
+
+}
