dbd08752d2cf9a93855784af97a7d51ea9693135
c1i2b <jeremylsellors@gmail.com>
2020-11-19 18:26:23 -0800

    ClassNode created

---
+++ src/Main/ClassNode.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class ClassNode {
+
+    private ArrayList<String> classesUsedAsFields;
+    private ArrayList<String> parentClassList;
+    private ArrayList<String> parentInterfaceList;
+    private ArrayList<String> classesReturnedByMethodsList;
+    private ArrayList<String> classesUsedAsLocalVariables;
+    private ArrayList<String> classesUsedAsArguments;
+}
