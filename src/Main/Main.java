c1c38f14113696c80904d865dbc563cf60a85c98
Yukie <yukie.man97@gmail.com>
2020-11-27 06:15:20 -0800

    * added hover feature for children private and public classes * method names of children private and public classes will appear after hover * made private classes the same colour (Color.ROYALBLUE) * made public classes the same colour (Color.SEAGREEN) * added comments

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,10 +5,11 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 import javafx.scene.shape.StrokeType;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -59,31 +60,74 @@ public class Main extends Application {
         scene.addEventFilter(MouseEvent.MOUSE_DRAGGED, sceneActions.getOnMouseDraggedEventHandler());
         scene.addEventFilter(ScrollEvent.ANY, sceneActions.getOnScrollEventHandler());
 
-        primaryStage.setTitle("Project 2");
+        primaryStage.setTitle("Circles Circles and Squares");
         primaryStage.setScene(scene);
         primaryStage.show();
 
 //        canvas.createGrid();
     }
 
-    public void drawShapes(Pane canvas, Group root) {
+    public void drawShapes(AnchorPane canvas, Group root) {
+        Color circle1HoverColor = Color.HONEYDEW;
+        Color publicColor = Color.SEAGREEN;
+        Color privateColor = Color.ROYALBLUE;
+        // create parent circle 1, which is basically the first class
+        Color circle1ChildrenColor = Color.LIGHTSALMON;
         Circle circle1 = createCircle(100, 100, P_CIRCLE_RADIUS, Color.VIOLET);
         Text text1 = createText(100, 100, "Circle 1");
-        Circle publicCircle1a = createPublicCircle(100, 100, C_CIRCLE_RADIUS, Color.LIGHTSALMON, circle1, text1);
-        Rectangle privateSquare1b = createPrivateSquare(100, 100, C_SQUARE_SIZE, Color.LIGHTSALMON, circle1, text1);
-        Circle publicCircle1c = createPublicCircle(100, 100, C_CIRCLE_RADIUS, Color.LIGHTSALMON, circle1, text1);
+
+        // create first child public class circle and text
+        Circle publicCircle1a = createPublicCircle(100, 100, C_CIRCLE_RADIUS, publicColor, circle1, text1);
+        Text text1a = createText(100, 100, "public method 1");
+
+        // create second child private class square and text
+        Rectangle privateSquare1b = createPrivateSquare(100, 100, C_SQUARE_SIZE, privateColor, circle1, text1);
+        Text text1b = createText(100, 100, "private method 1");
+
+        // create third child public class circle and text
+        Circle publicCircle1c = createPublicCircle(100, 100, C_CIRCLE_RADIUS, publicColor, circle1, text1);
+        Text text1c = createText(100, 100, "public method 2");
+
+        // add all nodes related to the first class to the canvas
         canvas.getChildren().addAll(circle1, text1, publicCircle1a, privateSquare1b, publicCircle1c);
 
+
+        // create parent circle 2, which is basically the second class
         Circle circle2 = createCircle(500, 100, P_CIRCLE_RADIUS, Color.CORNFLOWERBLUE);
         Text text2 = createText(500, 100, "Circle 2");
-        Rectangle privateSquare2a = createPrivateSquare(500, 100, C_SQUARE_SIZE, Color.MEDIUMORCHID, circle2, text2);
+
+        // create first child private class square and text
+        Rectangle privateSquare2a = createPrivateSquare(500, 100, C_SQUARE_SIZE, privateColor, circle2, text2);
+        Text text2a = createText(500, 100, "private method 1");
+
+        // add all nodes related to the second class to the canvas
         canvas.getChildren().addAll(circle2, text2, privateSquare2a);
 
+
+        // create parent circle 3, which is basically the third class
         Circle circle3 = createCircle(900, 100, P_CIRCLE_RADIUS, Color.CRIMSON);
         Text text3 = createText(900, 100, astp.getRepresentations().get("Program").getName());
-        Circle publicCircle3a = createPublicCircle(900, 100, C_CIRCLE_RADIUS, Color.PEACHPUFF, circle3, text3);
-        Rectangle privateSquare3b = createPrivateSquare(900, 100, C_SQUARE_SIZE, Color.PEACHPUFF, circle3, text3);
+
+        // create first child public class circle and text
+        Circle publicCircle3a = createPublicCircle(900, 100, C_CIRCLE_RADIUS, publicColor, circle3, text3);
+        Text text3a = createText(900, 100, "public method 1");
+
+        // create second child private class square and text
+        Rectangle privateSquare3b = createPrivateSquare(900, 100, C_SQUARE_SIZE, privateColor, circle3, text3);
+        Text text3b = createText(900, 100, "private method 1");
+
+        // add all nodes related to the second class to the canvas
         canvas.getChildren().addAll(circle3, text3, publicCircle3a, privateSquare3b);
+
+
+        // handles hovering over children nodes
+        registerHandler(canvas, publicCircle1a, circle1ChildrenColor,circle1HoverColor, text1a);
+        registerHandler(canvas, privateSquare1b, circle1ChildrenColor, circle1HoverColor, text1b);
+        registerHandler(canvas, publicCircle1c, circle1ChildrenColor, circle1HoverColor, text1c);
+        registerHandler(canvas, privateSquare2a, circle1ChildrenColor, circle1HoverColor, text2a);
+        registerHandler(canvas, publicCircle3a, circle1ChildrenColor, circle1HoverColor, text3a);
+        registerHandler(canvas, privateSquare3b, circle1ChildrenColor, circle1HoverColor, text3b);
+
 
         // ignore for now, allows us to move the circle itself
 //        circle1.addEventFilter(MouseEvent.MOUSE_PRESSED, nodeActions.getOnMousePressedEventHandler());
@@ -96,21 +140,22 @@ public class Main extends Application {
 
     // create a public circle to represent public methods of the parent class
     private Circle createPublicCircle(double x, double y, double radius, Color c, Circle parentCircle, Text text) {
+        // create circle
         final Circle privateCircle = new Circle(x, y, radius, c);
         privateCircle.setStroke(Color.GRAY);
         privateCircle.opacityProperty().set(0.6);
         privateCircle.setStrokeWidth(2.5);
         privateCircle.setStrokeType(StrokeType.INSIDE);
 
+        // set up for random spawning within the parent
         double textW = text.getBoundsInLocal().getWidth();
-        double textH = text.getBoundsInLocal().getHeight();
         double circleRadius = parentCircle.getRadius();
         double min = -1 * (circleRadius - (circleRadius*0.5));
         double max = circleRadius - (circleRadius*0.5);
         double randomX = ThreadLocalRandom.current().nextDouble(min, max);
         double randomY = ThreadLocalRandom.current().nextDouble(min, max);
 
-        // randomX and randomY positions must be within circle and not completely overlapping the text
+        // randomX and randomY positions must be within parent circle and not completely overlapping the text
         while (randomX > -1*(textW/2) && randomX < (textW/2)) {
             randomX = ThreadLocalRandom.current().nextDouble(min, max);
         }
@@ -125,6 +170,7 @@ public class Main extends Application {
 
     // create a private square to represent private methods of parent class
     private Rectangle createPrivateSquare(double x, double y, double radius, Color c, Circle parentCircle, Text text) {
+        // create square
         final Rectangle privateSquare = new Rectangle(x, y, radius, radius);
         privateSquare.setFill(c);
         privateSquare.setStroke(Color.GRAY);
@@ -132,6 +178,7 @@ public class Main extends Application {
         privateSquare.setStrokeWidth(2.5);
         privateSquare.setStrokeType(StrokeType.INSIDE);
 
+        // set up for random spawning within parent circle
         double textW = text.getBoundsInLocal().getWidth();
         double circleRadius = parentCircle.getRadius();
         double min = -1 * (circleRadius - (circleRadius*0.5));
@@ -139,7 +186,7 @@ public class Main extends Application {
         double randomX = ThreadLocalRandom.current().nextDouble(min, max);
         double randomY = ThreadLocalRandom.current().nextDouble(min, max);
 
-        // randomX and randomY positions must be within circle and not completely overlapping the text
+        // randomX and randomY positions must be within parent circle and not completely overlapping the text
         while (randomX > -1*(textW/2) && randomX < (textW/2)) {
             randomX = ThreadLocalRandom.current().nextDouble(min, max);
         }
@@ -155,7 +202,7 @@ public class Main extends Application {
     private Text createText(double x, double y, String s) {
         final Text text = new Text(s);
 
-        text.setFont(new Font(15));
+        text.setFont(new Font("Tahoma",15));
         text.setBoundsType(TextBoundsType.VISUAL);
         centerText(text, x, y);
 
@@ -175,5 +222,52 @@ public class Main extends Application {
         circle.setStrokeWidth(5);
         circle.setStrokeType(StrokeType.INSIDE);
         return circle;
+    }
+
+    // when mouse is hovered over shape, its color and size changes
+    // when mouse is not hovered over shape, its original color and size are restored
+    private void registerHandler(AnchorPane canvas, Shape s, Color defaultColor, Color hoverColor, Text text) {
+        double sizeChange = 5;
+        double textX = text.getX();
+        double textY = text.getY();
+        s.setOnMouseEntered( mouseEvent -> {
+            s.setFill(hoverColor);
+            if (s instanceof Circle) {
+                // increase size of circle
+                double radius = ((Circle) s).getRadius();
+                ((Circle) s).setRadius(radius + sizeChange);
+            } else if (s instanceof  Rectangle) {
+                // increase size of square
+                double width = ((Rectangle) s).getWidth();
+                double height = ((Rectangle) s).getHeight();
+                ((Rectangle) s).setWidth(width + sizeChange);
+                ((Rectangle) s).setHeight(height + sizeChange);
+            }
+            // shows name of method
+            // problem: keeps updating when mouse is close to text
+            text.setX(textX);
+            text.setY(textY);
+            text.setFont(new Font("Arial Black", 30));
+            text.setFill(Color.MISTYROSE);
+            text.setStroke(Color.BLACK);
+            text.setStrokeWidth(1.5);
+            canvas.getChildren().add(text);
+        });
+        s.setOnMouseExited(mouseEvent -> {
+            s.setFill(defaultColor);
+            if (s instanceof Circle) {
+                // decrease size of circle
+                double radius = ((Circle) s).getRadius();
+                ((Circle) s).setRadius(radius - sizeChange);
+            } else if (s instanceof  Rectangle) {
+                // decrease size of square
+                double width = ((Rectangle) s).getWidth();
+                double height = ((Rectangle) s).getHeight();
+                ((Rectangle) s).setWidth(width - sizeChange);
+                ((Rectangle) s).setHeight(height - sizeChange);
+            }
+            // doesn't show name of method anymore
+            canvas.getChildren().remove(text);
+        });
     }
 }
